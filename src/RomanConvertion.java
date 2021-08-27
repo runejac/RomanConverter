4fa2520d439460352d620cfd840b9bf70d050234
runejac <69790920+runejac@users.noreply.github.com>
2021-08-27 13:42:18 +0200

    shouldConvert5ToV to shouldConvert10ToX (passes)

--- src/RomanConvertion.java
+++ src/RomanConvertion.java
@@ -2,10 +2,21 @@ public class RomanConvertion {
 
     public static String toRoman(int i){
         String romanNumber = "";
-        if (i == 1) {
-            romanNumber = "I";
-        } else if (i == 2) {
-            romanNumber = "II";
+
+        String[] oners = {"I", "II", "III"};
+        String[] fourToThen = {"IV", "V", "VI", "VII", "VIII", "IX", "X"};
+
+
+
+        if (i >= 4 && i <= 10) {
+            for (int j = 0; j < fourToThen.length; j++) {
+                romanNumber = fourToThen[i-4];
+            }
+        }
+        if (i <= 3) {
+            for (int j = 0; j < oners.length; j++) {
+                romanNumber = oners[i-1];
+            }
         }
         return romanNumber;
     }
--- test/ConvertionTests.java
+++ test/ConvertionTests.java
@@ -14,4 +14,43 @@ public class ConvertionTests {
         assertEquals("II", RomanConvertion.toRoman(2));
     }
 
+    @Test
+    void shouldConvert3ToIII() {
+        assertEquals("III", RomanConvertion.toRoman(3));
+    }
+
+    @Test
+    void shouldConvert4ToIV() {
+        assertEquals("IV", RomanConvertion.toRoman(4));
+    }
+
+    @Test
+    void shouldConvert5ToV() {
+        assertEquals("V", RomanConvertion.toRoman(5));
+    }
+
+    @Test
+    void shouldConvert6ToVI() {
+        assertEquals("VI", RomanConvertion.toRoman(6));
+    }
+
+    @Test
+    void shouldConvert7ToVII() {
+        assertEquals("VII", RomanConvertion.toRoman(7));
+    }
+
+    @Test
+    void shouldConvert8ToVIII() {
+        assertEquals("VIII", RomanConvertion.toRoman(8));
+    }
+
+    @Test
+    void shouldConvert9ToIX() {
+        assertEquals("IX", RomanConvertion.toRoman(9));
+    }
+
+    @Test
+    void shouldConvert10ToX() {
+        assertEquals("X", RomanConvertion.toRoman(10));
+    }
 }
