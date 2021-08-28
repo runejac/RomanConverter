ffcbb3335adbc2822844a672561d6a255d4f7a14
runejac <69790920+runejac@users.noreply.github.com>
2021-08-29 00:47:02 +0200

    added /out on gitignore

--- src/RomanConvertion.java
+++ src/RomanConvertion.java
@@ -2,6 +2,8 @@ public class RomanConvertion {
 
     public static String toRoman(int i){
 
+        // egen info: Det er out-mappen han sa at vi skulle adde til git-ignore, ellers må man levere arbeidskrav på nytt =)
+
         StringBuilder inRoman = new StringBuilder();
         int[] numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
