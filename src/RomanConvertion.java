39991b97d4461a7b4c8dc4c02a5b870c3543e5df
runejac <69790920+runejac@users.noreply.github.com>
2021-08-28 17:48:11 +0200

    made one for loop instead of two, zero to x (passes)

--- src/RomanConvertion.java
+++ src/RomanConvertion.java
@@ -1,21 +1,14 @@
 public class RomanConvertion {
 
     public static String toRoman(int i){
+
         String romanNumber = "";
 
-        String[] oners = {"I", "II", "III"};
-        String[] fourToThen = {"IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        String[] romanNumerals = {"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
 
-
-
-        if (i >= 4 && i <= 10) {
-            for (int j = 0; j < fourToThen.length; j++) {
-                romanNumber = fourToThen[i-4];
-            }
-        }
-        if (i <= 3) {
-            for (int j = 0; j < oners.length; j++) {
-                romanNumber = oners[i-1];
+        if (i >= 0 && i <= 10) {
+            for (int j = 0; j < romanNumerals.length; j++) {
+                romanNumber = romanNumerals[i];
             }
         }
         return romanNumber;
